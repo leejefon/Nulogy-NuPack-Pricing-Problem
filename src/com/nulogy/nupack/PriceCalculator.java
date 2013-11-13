3a18a97331c69a00e449f79fc4852d81d645cf45
Jeff Lee @ N61Jv <leejefon@gmail.com>
2013-11-12 19:13:07 -0500

    Added test for simple argument checking

--- src/com/nulogy/nupack/PriceCalculator.java
+++ src/com/nulogy/nupack/PriceCalculator.java
@@ -16,6 +16,10 @@ public class PriceCalculator {
 	public double calculate(double basePrice, int numWorkers, Markup type) {
 		double finalPrice;
 
+		if (basePrice < 0 || numWorkers < 0) {
+			throw new IllegalArgumentException("Argument basePrice and numWorkers should not be less than 0");
+		}
+
 		finalPrice = basePrice * (1 + Markup.FLAT.markup());
 
 		finalPrice *= (1 + numWorkers * Markup.WORKER.markup() + type.markup());
--- test/com/nulogy/nupack/PriceCalculatorTest.java
+++ test/com/nulogy/nupack/PriceCalculatorTest.java
@@ -7,6 +7,8 @@
 package com.nulogy.nupack;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 
 /**
@@ -17,33 +19,46 @@ public class PriceCalculatorTest extends TestCase {
 
 	private PriceCalculator price;
 
+	public ExpectedException exception = ExpectedException.none();
+
 	@Override
 	public void setUp() {
 		price = new PriceCalculator();
 	}
 
-	public void testCreateCostCalculatorObject() {
+	public void testCreateCostCalculatorObject() throws Exception {
 		assertNotNull(price);
 	}
 
-	public void testCalculateFlatMarkup() {
+	public void testCalculateFlatMarkup() throws Exception {
 		assertEquals(105.00, price.calculate(100, 0, Markup.ELSE));
 	}
 
-	public void testCalculateWorkerMarkup() {
+	public void testCalculateWorkerMarkup() throws Exception {
 		assertEquals(108.78, price.calculate(100, 3, Markup.ELSE));
 		assertEquals(111.30, price.calculate(100, 5, Markup.ELSE));
 	}
 
-	public void testCalculateMaterialTypeMarkup() {
+	public void testCalculateMaterialTypeMarkup() throws Exception {
 		assertEquals(112.88, price.calculate(100, 0, Markup.PHARMACEUTICAL));
 		assertEquals(118.65, price.calculate(100, 0, Markup.FOOD));
 		assertEquals(107.10, price.calculate(100, 0, Markup.ELECTRONICS));
 	}
 
-	public void testCalculateAllMarkup() {
+	public void testCalculateAllMarkup() throws Exception {
 		assertEquals(1591.58, price.calculate(1299.99, 3, Markup.FOOD));
 		assertEquals(6199.81, price.calculate(5432.00, 1, Markup.PHARMACEUTICAL));
 		assertEquals(13707.63, price.calculate(12456.95, 4, Markup.ELSE));
+	}
+
+	public void testInvalidInputHandling() throws Exception {
+		try {
+			price.calculate(100, -1, Markup.ELSE);
+			fail("should have thrown exception IllegalArgumentException");
+		} catch(IllegalArgumentException e) {
+
+		} catch (Exception e) {
+			fail("throw wrong exception: " + e);
+		}
 	}
 }
