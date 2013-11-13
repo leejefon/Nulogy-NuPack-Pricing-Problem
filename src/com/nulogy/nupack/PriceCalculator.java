399c163339c317345d6e8d74fc964c841fe5554e
Jeff Lee @ N61Jv <leejefon@gmail.com>
2013-11-12 22:30:59 -0500

    Refactor the calculate method and break down to flatMarkup and otherMarkup

--- src/com/nulogy/nupack/PriceCalculator.java
+++ src/com/nulogy/nupack/PriceCalculator.java
@@ -13,6 +13,13 @@ package com.nulogy.nupack;
  */
 public class PriceCalculator {
 
+	/**
+	 *
+	 * @param basePrice
+	 * @param numWorkers
+	 * @param type
+	 * @return
+	 */
 	public double calculate(double basePrice, int numWorkers, Markup type) {
 		double finalPrice;
 
@@ -20,10 +27,26 @@ public class PriceCalculator {
 			throw new IllegalArgumentException("Argument basePrice and numWorkers should not be less than 0");
 		}
 
-		finalPrice = basePrice * (1 + Markup.FLAT.markup());
-
-		finalPrice *= (1 + numWorkers * Markup.WORKER.markup() + type.markup());
+		finalPrice = basePrice * flatMarkup() * otherMarkup(numWorkers, type);
 
 		return (double) Math.round(finalPrice * 100) / 100;
+	}
+
+	/**
+	 *
+	 * @return  The multiply factor with flatMarkup
+	 */
+	protected double flatMarkup() {
+		return 1 + Markup.FLAT.markup();
+	}
+
+	/**
+	 *
+	 * @param numWorkers
+	 * @param type
+	 * @return  The Multiply factor with number of workers markup and the materials type markup
+	 */
+	protected double otherMarkup(int numWorkers, Markup type) {
+		return 1 + numWorkers * Markup.WORKER.markup() + type.markup();
 	}
 }
